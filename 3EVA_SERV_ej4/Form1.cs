0850495e38765ff2f8e87637ac31faa512162284
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-05-18 23:22:55 +0200

    Add files via upload

--- 3EVA_SERV_ej4/Form1.cs
+++ 3EVA_SERV_ej4/Form1.cs
@@ -13,7 +13,8 @@ namespace _3EVA_SERV_ej4
 {
     public partial class Form1 : Form
     {
-        Process[] proccess;
+        Process[] processes;
+        Process proceso;
         public Form1()
         {
             InitializeComponent();
@@ -26,42 +27,98 @@ namespace _3EVA_SERV_ej4
 
         private void button1_Click(object sender, EventArgs e)
         {
-            getProcesses();
+            GetProcesses();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            GetProcessInfo();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            CloseProcess();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-
+            KillProcess();
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-
+            RunApp();
         }
 
-        private void getProcesses()
+        private void GetProcesses()
         {
-            proccess = Process.GetProcesses();
-            
-            foreach (Process p in proccess)
+            processes = Process.GetProcesses();
+
+            foreach (Process p in processes)
             {
-                textBox1.AppendText(p.ProcessName + "\n");
+                textBox1.AppendText(p.Id + "\t" + Acortar(p.ProcessName, 15) + "\t" + p.MainWindowTitle + "\r\n");
             }
         }
 
-        private void killProcess()
+        private void GetProcessInfo()
         {
-            
+            try
+            {
+                int pid = Convert.ToInt32(textBox2.Text);
+                proceso = Process.GetProcessById(pid);
+            }
+            catch (FormatException)
+            {
+
+            }
+
+            textBox1.ResetText();
+            textBox1.Text = String.Format("{0}\r\n{1}\r\n{2}\r\n{3}", proceso.Id, proceso.StartTime, proceso.MainModule, proceso.ProcessName);
+        }
+
+        private void CloseProcess()
+        {
+            try
+            {
+                int pid = Convert.ToInt32(textBox2.Text);
+                proceso = Process.GetProcessById(pid);
+            }
+            catch (FormatException)
+            {
+
+            }
+
+            proceso.Close();
+        }
+
+        private void KillProcess()
+        {
+            try
+            {
+                int pid = Convert.ToInt32(textBox2.Text);
+                proceso = Process.GetProcessById(pid);
+            }
+            catch (FormatException)
+            {
+
+            }
+
+            proceso.Kill();
+        }
+
+        private void RunApp()
+        {
+            string appName = textBox2.Text;
+            proceso = Process.Start(appName);
+        }
+
+        private string Acortar(string cadena, int longitud)
+        {
+            if (!string.IsNullOrEmpty(cadena) && cadena.Length > longitud)
+            {
+                return cadena.Substring(0, longitud) + "...";
+            }
+            return cadena;
         }
     }
 }
