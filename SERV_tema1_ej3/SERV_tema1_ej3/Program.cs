a2ce722c48f456eb6cab58d333a588d77514d952
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-10-19 12:30:31 +0200

    Add files via upload

---
+++ SERV_tema1_ej3/SERV_tema1_ej3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace SERV_tema1_ej3
+{
+    class Program
+    {
+        static int num = 0;
+        static bool finished = false;
+
+        public static void increment()
+        {
+            if (num == 1000 || num == -1000)
+            {
+                finished = true;
+            }
+
+            while (num <= 1000)
+            {
+                Console.WriteLine(num);
+                num++;
+            }
+        }
+
+        public static void decrement()
+        {
+            if (num == 1000 || num == -1000)
+            {
+                finished = true;
+            }
+
+            while (num >= -1000)
+            {
+                Console.WriteLine(num);
+                num--;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Program ej3 = new Program();
+            Thread tIncrease = new Thread(increment);
+            Thread tDecrease = new Thread(decrement);
+
+            tIncrease.Start();
+            tDecrease.Start();
+
+            if (finished)
+            {
+                tIncrease.Join();
+                tDecrease.Join();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
