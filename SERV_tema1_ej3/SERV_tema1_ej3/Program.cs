1c5ea576f1cc6842f672a9cf77ab4288b512c42d
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-10-20 11:20:58 +0200

    Add files via upload

--- SERV_tema1_ej3/SERV_tema1_ej3/Program.cs
+++ SERV_tema1_ej3/SERV_tema1_ej3/Program.cs
@@ -7,51 +7,77 @@ namespace SERV_tema1_ej3
     {
         static int num = 0;
         static bool finished = false;
+        static object l = new object();
 
         public static void increment()
         {
-            if (num == 1000 || num == -1000)
+            while (!finished)
             {
-                finished = true;
-            }
-
-            while (num <= 1000)
-            {
-                Console.WriteLine(num);
-                num++;
+                lock (l)
+                {
+                    Console.WriteLine("Incrementa {0}", num);
+                    num++;
+                    if (num == 1000)
+                    {
+                        finished = true;
+                    }
+                }
             }
         }
 
         public static void decrement()
         {
-            if (num == 1000 || num == -1000)
+            while (!finished)
             {
-                finished = true;
-            }
+                lock (l)
+                {
+                    Console.WriteLine("Decrementa {0}", num);
+                    num--;
 
-            while (num >= -1000)
-            {
-                Console.WriteLine(num);
-                num--;
+                    if (num == -1000)
+                    {
+                        finished = true;
+                    }
+                }
             }
         }
 
         static void Main(string[] args)
         {
-            //Program ej3 = new Program();
             Thread tIncrease = new Thread(increment);
             Thread tDecrease = new Thread(decrement);
 
             tIncrease.Start();
             tDecrease.Start();
 
+            Console.ReadKey();
+
             if (finished)
             {
                 tIncrease.Join();
                 tDecrease.Join();
-            }
 
-            Console.ReadKey();
+                if (num == 999)
+                {
+                    num++;
+                }
+
+                if (num == -999)
+                {
+                    num--;
+                }
+
+                if (num == 1000)
+                {
+                    Console.WriteLine("\nGANADOR: INCREMENTO");
+                }
+                else
+                {
+                    Console.WriteLine("\nGANADOR: DECREMENTO");
+                }
+
+                Console.WriteLine(num);
+            }
         }
     }
 }
