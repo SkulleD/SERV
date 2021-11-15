cf7f0e4c81ca0bed48ff76d5cd62b61ff650685b
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-11-15 13:58:03 +0100

    Add files via upload

--- SERV_tema1_ej4/SERV_tema1_ej4/Caballo.cs
+++ SERV_tema1_ej4/SERV_tema1_ej4/Caballo.cs
@@ -8,17 +8,23 @@ namespace SERV_tema1_ej4
     {
         public int Position { set; get; }
         public string Name{ set; get; }
+        public int Y { set; get; }
 
-        public Caballo(string name)
+        public Caballo(string name, int y)
         {
             this.Position = 0;
             this.Name = name;
+            this.Y = y;
         }
 
         public int Correr()
         {
             Random random = new Random();
             this.Position += random.Next(1, 2);
+            if (Position > 50)
+            {
+                Position = 50;
+            }
             return Position;
         }
     }
--- SERV_tema1_ej4/SERV_tema1_ej4/Program.cs
+++ SERV_tema1_ej4/SERV_tema1_ej4/Program.cs
@@ -5,149 +5,32 @@ namespace SERV_tema1_ej4
 {
     class Program
     {
-        static Caballo h1 = new Caballo("Horse 1");
-        static Caballo h2 = new Caballo("Horse 2");
-        static Caballo h3 = new Caballo("Horse 3");
-        static Caballo h4 = new Caballo("Horse 4");
-        static Caballo h5 = new Caballo("Horse 5");
-
-        Caballo[] caballos = { h1, h2, h3, h4, h5 };
-
-        static Thread correr1;
-        static Thread correr2;
-        static Thread correr3;
-        static Thread correr4;
-        static Thread correr5;
+        Caballo[] caballos;
 
         static object l = new object();
         bool meta = false;
         string winner = "";
 
-        public void Correr1() // HACER UN DELEGADO PARA NO TENER TANTOS METODOS
+        public void CorrerMain(object i)
         {
             while (!meta)
             {
                 lock (l)
                 {
                     Monitor.Pulse(l);
-                    if (!meta)
+                    if (((Caballo)i).Position < 50 && !meta)
                     {
-                        if (h1.Position <= 50)
-                        {
-                            Console.SetCursorPosition(h1.Correr(), 0);
-                            Console.Write("*");
-                            Thread.Sleep(50);
-                            Monitor.Wait(l);
-                        }
-                        else
-                        {
-                            winner = h1.Name;
-                            meta = true;
-                        }
+                        Console.SetCursorPosition(((Caballo)i).Correr(), ((Caballo)i).Y);
+                        Console.Write("*");
+                        Thread.Sleep(50);
+                        Monitor.Wait(l);
                     }
-                }
-            }
-        }
-
-        public void Correr2()
-        {
-            while (!meta)
-            {
-                lock (l)
-                {
-                    Monitor.Pulse(l);
-                    if (!meta)
+                    else
                     {
-                        if (h2.Position <= 50)
-                        {
-                            Console.SetCursorPosition(h2.Correr(), 1);
-                            Console.Write("*");
-                            Thread.Sleep(50);
-                            Monitor.Wait(l);
-                        }
-                        else
-                        {
-                            winner = h2.Name;
-                            meta = true;
-                        }
-                    }
-                }
-            }
-        }
-
-        public void Correr3()
-        {
-            while (!meta)
-            {
-                lock (l)
-                {
-                    Monitor.Pulse(l);
-                    if (!meta)
-                    {
-                        if (h3.Position <= 50)
-                        {
-                            Console.SetCursorPosition(h3.Correr(), 2);
-                            Console.Write("*");
-                            Thread.Sleep(50);
-                            Monitor.Wait(l);
-                        }
-                        else
-                        {
-                            winner = h3.Name;
-                            meta = true;
-                        }
-                    }
-                }
-            }
-        }
-
-        public void Correr4()
-        {
-            while (!meta)
-            {
-                lock (l)
-                {
-                    Monitor.Pulse(l);
-                    if (!meta)
-                    {
-                        if (h4.Position <= 50)
-                        {
-                            Console.SetCursorPosition(h4.Correr(), 3);
-                            Console.Write("*");
-                            Thread.Sleep(50);
-                            Monitor.Wait(l);
-                        }
-                        else
-                        {
-                            winner = h4.Name;
-                            meta = true;
-                        }
-                    }
-                }
-            }
-        }
-
-        public void Correr5()
-        {
-            while (!meta)
-            {
-                lock (l)
-                {
-                    Monitor.Pulse(l);
-                    if (!meta)
-                    {
-                        if (h5.Position <= 50)
-                        {
-                            Console.SetCursorPosition(h5.Correr(), 4);
-                            Console.Write("*");
-                            Thread.Sleep(50);
-                            Monitor.Wait(l);
-                        }
-                        else
-                        {
-                            winner = h5.Name;
-                            meta = true;
-                        }
+                        winner = ((Caballo)i).Name;
+                        Console.SetCursorPosition(0, caballos.Length);
+                        Console.WriteLine($"\n----> The winner is {winner}!");
+                        meta = true;
                     }
                 }
             }
@@ -155,37 +38,27 @@ namespace SERV_tema1_ej4
 
         public void StartCarrera()
         {
-            correr1 = new Thread(Correr1);
-            correr2 = new Thread(Correr2);
-            correr3 = new Thread(Correr3);
-            correr4 = new Thread(Correr4);
-            correr5 = new Thread(Correr5);
-            Thread[] alvaroFlipalo = new Thread[5];
+            Thread[] carrera = new Thread[5];
+            Caballo caballo;
+            caballos = new Caballo[5];
+            string name = "1";
 
-            Thread[] carrera = { correr1, correr2, correr3, correr4, correr5 };
-
-            for (int i = 0; i < carrera.Length; i++)
+            for (int i = 0; i < caballos.Length; i++)
             {
-                //alvaroFlipalo[i] = new Thread(Correr1(i));
-                carrera[i].Start();
+                caballo = new Caballo("" + i, i);
+                caballos[i] = caballo;
+                carrera[i] = new Thread(CorrerMain);
+                carrera[i].Start(caballos[i]);
+                name = "" + i;
             }
 
-            correr1.Join();
-            correr2.Join();
-            correr3.Join();
-            correr4.Join();
-            correr5.Join();
+
         }
 
         static void Main(string[] args)
         {
             Program ej4 = new Program();
             ej4.StartCarrera();
-
-
-
-            Console.SetCursorPosition(0, ej4.caballos.Length);
-            Console.WriteLine($"\n----> The winner is {ej4.winner}!");
         }
     }
 }
