0aef18b8ee0dc28ad564354ab3fe24227110e75e
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-12-13 13:50:36 +0100

    Add files via upload

---
+++ SERV_tema2_ej2/SERV_tema2_ej2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SERV_tema2_ej2
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Exit program?", "SERV Tema2 ej2",
+                 MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
