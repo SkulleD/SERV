13c8f7935aca0c159abf6f6d78093f11774bef22
Álvaro <51362780+SkulleD@users.noreply.github.com>
2021-12-16 12:26:46 +0100

    Add files via upload

--- SERV_tema2_ej2/SERV_tema2_ej2/Form1.Designer.cs
+++ SERV_tema2_ej2/SERV_tema2_ej2/Form1.Designer.cs
@@ -30,22 +30,111 @@ namespace SERV_tema2_ej2
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtDirectorio = new System.Windows.Forms.TextBox();
+            this.txtPalabra = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.checkMayus = new System.Windows.Forms.CheckBox();
+            this.txtMultiline = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // txtDirectorio
+            // 
+            this.txtDirectorio.Location = new System.Drawing.Point(12, 23);
+            this.txtDirectorio.Name = "txtDirectorio";
+            this.txtDirectorio.Size = new System.Drawing.Size(237, 20);
+            this.txtDirectorio.TabIndex = 0;
+            // 
+            // txtPalabra
+            // 
+            this.txtPalabra.Location = new System.Drawing.Point(12, 71);
+            this.txtPalabra.Name = "txtPalabra";
+            this.txtPalabra.Size = new System.Drawing.Size(120, 20);
+            this.txtPalabra.TabIndex = 1;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(162, 66);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(87, 29);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "SEARCH";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // checkMayus
+            // 
+            this.checkMayus.AutoSize = true;
+            this.checkMayus.Location = new System.Drawing.Point(162, 111);
+            this.checkMayus.Name = "checkMayus";
+            this.checkMayus.Size = new System.Drawing.Size(101, 17);
+            this.checkMayus.TabIndex = 3;
+            this.checkMayus.Text = "Mayus sensitive";
+            this.checkMayus.UseVisualStyleBackColor = true;
+            // 
+            // txtMultiline
+            // 
+            this.txtMultiline.Location = new System.Drawing.Point(317, 23);
+            this.txtMultiline.Multiline = true;
+            this.txtMultiline.Name = "txtMultiline";
+            this.txtMultiline.ReadOnly = true;
+            this.txtMultiline.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMultiline.Size = new System.Drawing.Size(309, 406);
+            this.txtMultiline.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 152);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(290, 277);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(9, 7);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(35, 13);
+            this.lblInfo.TabIndex = 6;
+            this.lblInfo.Text = "label1";
             // 
             // Form1
             // 
+            this.AcceptButton = this.btnBuscar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(645, 450);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtMultiline);
+            this.Controls.Add(this.checkMayus);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtPalabra);
+            this.Controls.Add(this.txtDirectorio);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Word-Searcher 3000";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtDirectorio;
+        private System.Windows.Forms.TextBox txtPalabra;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.CheckBox checkMayus;
+        private System.Windows.Forms.TextBox txtMultiline;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblInfo;
     }
 }
 
--- SERV_tema2_ej2/SERV_tema2_ej2/Form1.cs
+++ SERV_tema2_ej2/SERV_tema2_ej2/Form1.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,21 +15,93 @@ namespace SERV_tema2_ej2
 {
     public partial class Form1 : Form
     {
-
+        static object l = new object();
+        delegate void Buscador(string palabra, TextBox textbox);
+        Buscador buscador;
+        DirectoryInfo dirInfo;
+        Thread[] hilo;
+        ArrayList files = new ArrayList();
+        string directorio = "";
+        string palabra = "";
+        bool terminado = false;
 
         public Form1()
         {
             InitializeComponent();
+            directorio = Directory.GetCurrentDirectory();
+            pictureBox1.Image = Image.FromFile("C:\\Users\\AlvaroVila\\source\\repos\\SERV_tema2_ej2\\SERV_tema2_ej2\\bocatagarto.jpg");
+        }
+
+        private void FileCheck(string directorio, string palabra) // Comprueba que el directorio existe, encuentra los archivos de texto y llama a los hilos buscadores
+        {
+            this.directorio = directorio;
+            this.palabra = palabra;
+            string linea = "";
+            int numberThreads = 0;
+
+            if (Directory.Exists(directorio))
+            {
+                Directory.SetCurrentDirectory(directorio);
+                dirInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
+                lblInfo.Text = "Ta bien";
+                foreach (FileInfo file in dirInfo.GetFiles())
+                {
+                    try
+                    {
+                        Stream stream = new FileStream(file.FullName, FileMode.Open, FileAccess.Read);
+                        linea = "";
+
+                        for (int i = 0; i < stream.Length; i++) // Comprueba el archivo para ver si es de texto sencillo o no (solo vale texto sencillo)
+                        {
+                            int chara = stream.ReadByte();
+
+                            if (chara >= 0 && chara <= 127) // Si es un archivo con solo texto sencillo se añade al ArrayList de archivos de texto donde buscar la palabra
+                            {
+                                files.Add(file);
+                                numberThreads++;
+                            }
+                        }
+
+                        stream.Dispose();
+                    }
+                    catch (Exception ex) when (ex is FileNotFoundException || ex is DirectoryNotFoundException || ex is IOException || ex is UnauthorizedAccessException)
+                    {
+
+                    }
+                }
+            }
+
+            hilo = new Thread[numberThreads];
+
+            for (int i = 0; i < hilo.Length; i++)
+            {
+                hilo[i] = new Thread(HiloBuscador(files[i]));
+            }
+        }
+
+        private void HiloBuscador(File file) // Los hilos que buscan la palabra
+        {
+            while (!terminado)
+            {
+                lock (l)
+                {
+                    if (!terminado)
+                    {
+
+                    }
+                }
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            FileCheck(txtDirectorio.Text, txtPalabra.Text);
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (MessageBox.Show("Exit program?", "SERV Tema2 ej2",
-                 MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
-            {
-
-            }
-            else
+                 MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.Cancel)
             {
                 e.Cancel = true;
             }
