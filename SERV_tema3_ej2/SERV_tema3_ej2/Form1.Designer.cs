c1a2b36b85138485572c2fbc5eba3600944df91b
Álvaro <51362780+SkulleD@users.noreply.github.com>
2022-02-02 11:46:47 +0100

    Add files via upload

---
+++ SERV_tema3_ej2/SERV_tema3_ej2/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace SERV_tema3_ej2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.lbl_IP = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnList = new System.Windows.Forms.Button();
+            this.lbl_Info = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(12, 29);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(100, 20);
+            this.txtIP.TabIndex = 0;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(118, 29);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(55, 20);
+            this.txtPort.TabIndex = 1;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(12, 90);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 20);
+            this.txtUser.TabIndex = 2;
+            // 
+            // lbl_IP
+            // 
+            this.lbl_IP.AutoSize = true;
+            this.lbl_IP.Location = new System.Drawing.Point(12, 13);
+            this.lbl_IP.Name = "lbl_IP";
+            this.lbl_IP.Size = new System.Drawing.Size(17, 13);
+            this.lbl_IP.TabIndex = 3;
+            this.lbl_IP.Text = "IP";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(115, 13);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(26, 13);
+            this.lblPort.TabIndex = 4;
+            this.lblPort.Text = "Port";
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(12, 74);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(29, 13);
+            this.lblUser.TabIndex = 5;
+            this.lblUser.Text = "User";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(26, 132);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Visible = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(107, 132);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(75, 23);
+            this.btnList.TabIndex = 7;
+            this.btnList.Text = "LIST";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Visible = false;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // lbl_Info
+            // 
+            this.lbl_Info.AutoSize = true;
+            this.lbl_Info.Location = new System.Drawing.Point(85, 72);
+            this.lbl_Info.Name = "lbl_Info";
+            this.lbl_Info.Size = new System.Drawing.Size(108, 13);
+            this.lbl_Info.TabIndex = 8;
+            this.lbl_Info.Text = "(!) User disconnected";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(118, 88);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 9;
+            this.btnConnect.Text = "CONNECT";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(210, 287);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.lbl_Info);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.lbl_IP);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIP);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label lbl_IP;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Label lbl_Info;
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
+
---
+++ SERV_tema3_ej2/SERV_tema3_ej2/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SERV_tema3_ej2
+{
+    public partial class Form1 : Form
+    {
+        string ip;
+        int port;
+        bool connected = false; // Mientras sea true, los botones están activos
+        bool btnClick = false; // Mientras sea true, la conexión permanece activa
+        IPEndPoint ipEndPoint;
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Connection()
+        {
+            try
+            {
+                ip = txtIP.Text;
+                port = int.Parse(txtPort.Text);
+                ipEndPoint = new IPEndPoint(IPAddress.Parse(ip), 45);
+
+                socket.Connect(ipEndPoint);
+                connected = true;
+                lbl_Info.Text = "User connected!";
+            }
+            catch (SocketException e)
+            {
+                MessageBox.Show($"Error connection: {e.Message}\n Error code: {(SocketError)e.ErrorCode}({e.ErrorCode})", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            catch (FormatException e)
+            {
+                MessageBox.Show("Not valid IP!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            Connection();
+
+            if (connected)
+            {
+                btnAdd.Visible = true;
+                btnList.Visible = true;
+            }
+
+            while (!btnClick)
+            {
+
+            }
+
+            lbl_Info.Text = "(!) User disconnected";
+            btnAdd.Visible = false;
+            btnList.Visible = false;
+            connected = false;
+            socket.Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string msg = "add";
+            WriteMessage(msg);
+            btnClick = true;
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            string msg = "list";
+            WriteMessage(msg);
+            btnClick = true;
+        }
+
+        private void WriteMessage(string msg)
+        {
+            string msgServer;
+
+            using (NetworkStream netStream = new NetworkStream(socket))
+            using (StreamReader reader = new StreamReader(netStream))
+            using (StreamWriter writer = new StreamWriter(netStream))
+            {
+                writer.WriteLine($"User {txtUser.Text}");
+
+                msgServer = reader.ReadLine();
+
+                writer.WriteLine(msg);
+                writer.Flush();
+            }
+        }
+    }
+}
